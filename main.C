0e0ca15c98b8558d23100e0800e0f86cf8b15dbb
NellyMakena <nellysolomonmakena3@gmail.com>
2025-04-23 10:49:30 +0300

    project structure

---
+++ main.C
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+// Functions
+void add_task();
+void list_tasks();
+void mark_done();
+void delete_task();
+
+int main() {
+    int choice;
+    do {
+        printf("\nTask Manager\n");
+        printf("1. Add Task\n");
+        printf("2. List Tasks\n");
+        printf("3. Mark Task as Done\n");
+        printf("4. Delete Task\n");
+        printf("5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1: add_task(); break;
+            case 2: list_tasks(); break;
+            case 3: mark_done(); break;
+            case 4: delete_task(); break;
+            case 5: printf("Exiting...\n"); break;
+            default: printf("Invalid choice! Try again.\n");
+        }
+    } while (choice != 5);
+
+    return 0;
+}
+
+void add_task() {
+    // To be implemented by Mark
+}
+
+void list_tasks() {
+    // To be implemented by Scott 
+}
+
+void mark_done() {
+    // To be implemented by Erastus
+}
+
+void delete_task() {
+    // To be implemented by Erastus
+}
